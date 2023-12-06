ffeb60fab41510502cc5fdec92f3b00782ea095c
Anton Huck <anton.huck@snowflake.com>
2023-12-06 14:09:42 +0100

    Add Multipage Demo

---
+++ pages/Multipage_Demo.py
@@ -0,0 +1,15 @@
+# Import python packages
+import streamlit as st
+from snowflake.snowpark.context import get_active_session
+import snowflake.snowpark.functions as F
+
+# Write directly to the app
+st.title("🧾 This is a second page")
+st.write(
+    """Just for demo purposes.
+    """
+)
+
+session = get_active_session()
+
+st.ballons()
